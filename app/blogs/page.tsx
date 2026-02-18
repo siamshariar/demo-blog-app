ce237e29f998e9ba6900c6b66f4c2d4484e0725b
siamshariar <siam.it.int@gmail.com>
2026-02-18 18:55:05 +0600

    Update page.tsx

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -4,7 +4,7 @@ export default function BlogListPage() {
   return (
     <div className="min-h-screen bg-white">
       <div className="max-w-6xl mx-auto p-6 md:p-10">
-        <h1 className="text-4xl font-bold mb-10 text-center text-black">Latest Textile Insights</h1>
+        <h1 className="text-4xl font-bold mb-10 text-center text-black">Trending Posts</h1>
         <BlogList />
       </div>
     </div>
